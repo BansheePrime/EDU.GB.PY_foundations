b01863e850fa4625a335134a75db1078acce08fa
Banshee Prime <35134945+BansheePrime@users.noreply.github.com>
2022-03-01 09:08:33 +0300

    Update guess_computer.py

--- 07_lesson/guess_computer.py
+++ 07_lesson/guess_computer.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python3
+  GNU nano 5.6.1                                       guess_computer.py                                                 #!/usr/bin/env python3
 
 number = 50
-guess = int(input(f'Я попробую угадать ваше число. Вы загадали {number}? Нажмите "2", если я угадал. Нажмите "1", если ваше число больше. Нажмите "0", если ваше число меньше. '))
-if guess not in [0, 1, 2]:
-    guess = int(input(f'Вы набрали {guess}. Я ждал от вас 0, 1 или 2. Попробуйте еще раз: '))
+proper = [0, 1, 2]
+move = 0
+
+guess = int(input(f'Я попробую угадать ваше число. Вы загадали {number}? Нажмите "2", если я угадала. \nНажмите "1", есл>while guess not in proper:
+        guess = int(input(f'Вы набрали {guess}. Я ждала от вас 0, 1 или 2. Попробуйте еще раз: '))
+
+if guess == 2:
+        # print('Начнём угадывать ваше число.')
+        print(f'Я отгадала! Вы загадали {number}.')
+
+elif guess == 1:
+        print(f'Значит ваше число больше {number}? Хм, надо подумать ...')
+        number = (100 - number)//2 + number
+        guess = int(input(f'Я попробую угадать ваше число. Вы загадали {number}? Нажмите "2", если я угадала. \nНажмите >
+elif guess ==0:
+        print(f'Значит ваше число меньше {number}? Хм, надо подумать ...')
+
+else:
+        print('Thank you!')
 
 print(guess)
+print(number)
