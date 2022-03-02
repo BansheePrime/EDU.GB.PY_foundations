6e04c12936a920158ce1dd80a1334acbe2b9434e
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-02 10:00:35 +0300

    VSCode commit

--- homework_task_1.py
+++ homework_task_1.py
@@ -11,5 +11,3 @@ minutes = (duration - (days * 86400) - (hours * 3600)) // 60
 seconds = (duration - (days * 86400) - (hours * 3600) - (minutes * 60))
 
 print(f'{days} дн {hours} час {minutes} мин {seconds} сек')
-
-
