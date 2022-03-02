a57a6c9d712d48cf87fcf65f3e32c6d3aebb569c
gidrevichan <gidrevichan@mosmetod.ru>
2022-03-02 09:56:59 +0300

    fixing homework_task_1.py

--- homework_task_1.py
+++ homework_task_1.py
@@ -2,25 +2,14 @@
 # How long in took you?
 
 duration = int(input('Введите время в секундах для пересчета: '))
-print(f'Вы ввели {duration} сек.\n Подумаешь, бином Ньютона!\n ')
+print(f'Вы ввели {duration} сек.\nПодумаешь, бином Ньютона!\n ')
 days = hours = minutes = seconds = 0
 
-if duration // 60 < 1:
-    print(f'{duration} сек. Это много или мало?')
-elif 1 < (duration // 60) < 61:
-    minutes = duration // 60 
-    seconds = duration - (minutes * 60)
-    print(f'{minutes} мин и {seconds} сек.')
-elif 60 < (duration // 60) < 3601:
-    hours = duration // 360 ## ДОПИСАТЬ
-    minutes = duration // 60 ## ДОПИСАТЬ
-    seconds = duration - (minutes * 60)
-    print(f'{hours} час {minutes} мин {seconds} сек.')
-elif 60 < (duration // 60) < 3601:
-    days = duration // 3600 ## ДОПИСАТЬ
-    hours = duration // 360 ## ДОПИСАТЬ
-    minutes = duration // 60 ## ДОПИСАТЬ
-    seconds = duration - (minutes * 60)
-    print(f'{days} дн {hours} час {minutes} мин {seconds} сек.')
-else:
-    print(f'Извините, вы сказали {duration}. Хм, тут нужен суперкомпьютер, извините.')
+days = duration // 86400
+hours = (duration - (days * 86400)) // 3600
+minutes = (duration - (days * 86400) - (hours * 3600)) // 60
+seconds = (duration - (days * 86400) - (hours * 3600) - (minutes * 60))
+
+print(f'{days} дн {hours} час {minutes} мин {seconds} сек')
+
+
