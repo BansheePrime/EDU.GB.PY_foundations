5afe0e62907b92aa09f54f0b204117fcc7e2563b
Banshee Prime <35134945+BansheePrime@users.noreply.github.com>
2022-03-04 12:28:43 +0300

    Add files via upload
    adding bunch of files to project

---
+++ 2_homework_lesson02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Урок 2. Некоторые встроенные типы и операции с ними
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+new_list = []
+
+for i in my_list:
+    try:
+        int_value = int(i)
+    except ValueError:
+        pass
+    else:
+        new_list.append(i)
+
+print(new_list)
