15511043c20968e0c53c1c2ead95048f8f1b0594
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-07 13:46:21 +0300

    fixing 2 task and task 4 is finished

--- 2_homework_lesson02.py
+++ 2_homework_lesson02.py
@@ -2,7 +2,7 @@
 # Урок 2. Некоторые встроенные типы и операции с ними
 
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-new_list = []
+""" new_list = []
 
 for i in my_list:
     try:
@@ -13,3 +13,7 @@ for i in my_list:
         new_list.append(i)
 
 print(new_list)
+ """
+check_list = []
+
+
