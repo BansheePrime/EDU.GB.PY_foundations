d7dacc4f61cb20f92c1ce3c44066943893c79015
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-10 21:55:18 +0300

    adding solution for task 2 and updating other files

---
+++ 2_homework_lesson_03.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Урок 3. Функции. Словари
+
+dictionary_words = {
+    'zero': 'Ноль',
+    'one': 'Один',
+    'two': 'Два',
+    'three': 'Три',
+    'four': 'Четыре',
+    'five': 'Пять',
+    'six': 'Шесть',
+    'seven': 'Семь',
+    'eight': 'Восемь',
+    'nine': 'Девять',
+    'ten': 'Десять'
+}
+word_input = input('Введите, пожалуйста, словом число от 0 до 10 на английском языке: ')
+
+
+def num_translate():
+    # Перевод числительных на английский язык
+    return print(f'\"{dictionary_words.get(word_input.lower(), None)}\"')
+
+
+num_translate()
