a8030826f35a15a47917357d5bc6a62bae786c43
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-10 22:37:57 +0300

    fixing task 3, looks good now

--- 2_homework_lesson_03.py
+++ 2_homework_lesson_03.py
@@ -2,24 +2,27 @@
 # Урок 3. Функции. Словари
 
 dictionary_words = {
-    'zero': 'Ноль',
-    'one': 'Один',
-    'two': 'Два',
-    'three': 'Три',
-    'four': 'Четыре',
-    'five': 'Пять',
-    'six': 'Шесть',
-    'seven': 'Семь',
-    'eight': 'Восемь',
-    'nine': 'Девять',
-    'ten': 'Десять'
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
 }
 word_input = input('Введите, пожалуйста, словом число от 0 до 10 на английском языке: ')
 
 
 def num_translate():
-    # Перевод числительных на английский язык
-    return print(f'\"{dictionary_words.get(word_input.lower(), None)}\"')
+    # Перевод числительных на английский язык с учетом регистра
+    if word_input.istitle():
+        return print(f'\"{dictionary_words.get(word_input.lower(), None).capitalize()}\"')
+    else:
+        return print(f'\"{dictionary_words.get(word_input, None)}\"')
 
 
 num_translate()
