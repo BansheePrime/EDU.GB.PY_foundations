29df81c3b353eeebe2896c58b540320888e8408e
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-12 10:28:04 +0300

    first working example of task 5 , no repeat key and naming args yet to be done

--- 5_homework_lesson_03.py
+++ 5_homework_lesson_03.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Урок 3. Функции. Словари
 import random
+some_number = int(input('Введите желаемое количество шуток: '))
 
 nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
@@ -12,8 +13,12 @@ def get_jokes(some_number):
     :param words: количество шуток
     :return: anekdot.ru
     '''
+    i = 0
+    joker = []
+    for i in range(some_number):
+        joke = (f'{random.choice(nouns)} {random.choice(adverbs)} {random.choice(adjectives)}')
+        i += 1
+        joker.append(joke)
+    return print(joker)
 
-    joke = {random.choice(nouns)} {random.choice(adverbs)} {random.choice(adverbs)}
-    return print(joke)
-
-get_jokes()
+get_jokes(some_number)
