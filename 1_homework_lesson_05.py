7ea53da3dc833f875a123a4d3673698905108a35
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-03-30 21:54:40 +0300

    homework 1 and 2 done

--- 1_homework_lesson_05.py
+++ 1_homework_lesson_05.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+import sys
+
+
+def odd_generator(some_num):
+    for odd_number in range(1, some_num + 1, 2):
+        yield odd_number
+
+
+for number in odd_generator(15):
+    print(number)
--- 2_homework_lesson_05.py
+++ 2_homework_lesson_05.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python3
+num = int(input('Пожалуйста, введите целое число: '))
+odd_number = range(1, num, 2)
+for step in odd_number:
+    print(step)
