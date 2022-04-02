de085e06509efde085e0ada8bd834323c19b32a9
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-02 17:52:54 +0300

    task 3 is killing me I must confess

--- 3_homework_lesson_06.py
+++ 3_homework_lesson_06.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
+fio_file = open('users.csv')
+for line in fio_file:
+    full_name = (line.split(','))
+    print(full_name)
+fio_file.close()
 
+hobby_file = open('hobby.csv')
+for h_line in hobby_file:
+    hobby_name = (h_line.strip().split(','))
+    print(hobby_name)
+hobby_file.close()
+
+# useful_data = {}
+# useful_data = dict.fromkeys(full_name, hobby_name)
+# print(useful_data)
