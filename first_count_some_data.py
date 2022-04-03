148362e90a6a2d6b18eb8d63b45dbda56c201414
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-03 20:07:57 +0300

    adding cool counting scripts for files

---
+++ first_count_some_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Выводит на экран имена и размер файлов размером не более 15 КБ
+# при помощи функции listdir() и при помощи альтернативной функции scandir()
+
+import os
+from time import perf_counter
+
+folder = 'some_data'
+start = perf_counter()
+size_threshold = 15 * 2 ** 10
+small_files = [item
+               for item in os.listdir(folder)
+               if os.stat(os.path.join(folder, item)).st_size < size_threshold]
+print(len(small_files), perf_counter() - start)
+
+start = perf_counter()
+small_files_2 = [item.name
+                 for item in os.scandir(folder)
+                 if item.stat().st_size < size_threshold]
+print(len(small_files_2), perf_counter() - start)
+print(small_files == small_files_2)
