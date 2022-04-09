dd7d2345293ae2d7a3c2ea4672a6078b24884cb6
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-09 19:42:41 +0300

    task 2 fixing regex'es

--- 2_homework_lesson_08.py
+++ 2_homework_lesson_08.py
@@ -5,8 +5,8 @@ import re
 
 # Валидация ip4 адресов через regex требует особой тщательности
 # <remote_addr> "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}"gm
-# <request_datetime> "\[(.*?)\]"g
-# <request_type> "[A-Z]{3,}\s"gm "\"[A-Z]{3,}"g #второй ааприант лучше
-# <requested_resource> "(\/.{9}/.{9})"g ## очень плохно ориентироваться на кол-во символов здесь
-# <response_code> "\s\d{3}\s"gm
-# <response_size> "(\s\d{3}\s\d+)"gm ## потом взять только вторую часть
+# <request_datetime> "\[(.*?)\]"gm
+# <request_type> "\"([A-Z]{3,})"gm
+# <requested_resource> "(\/.{9}\/.{9})"gm ## очень плохо ориентироваться на кол-во символов
+# <response_code> "\s(\d{3})\s"gm
+# <response_size> "\d{3}\s(\d+)"gm
