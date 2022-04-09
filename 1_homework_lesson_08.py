652abe69598dc4b2774d373d8f3132f06fcf5930
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-09 20:27:31 +0300

    task 1 breaking on russian letters [tbd]

--- 1_homework_lesson_08.py
+++ 1_homework_lesson_08.py
@@ -2,3 +2,19 @@
 # Урок 8. Декораторы
 # Задание 1
 
+import re
+
+RE_EMAIL = re.compile(r'\b[\w.-]+?@\w+?\.\w+?\b')
+
+
+def email_parse(email_address):
+    """Парсер e-mail"""
+    return RE_EMAIL.match(email_address)
+
+
+if __name__ == '__main__':
+    while True:
+        email_address = input('Введите адрес для парсинга:\n')
+        if email_parse(email_address):
+            break
+    print(f'Результат:{email_address}')
