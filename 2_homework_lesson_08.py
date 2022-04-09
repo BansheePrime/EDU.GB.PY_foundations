4b4930b52c0dc30fc0911211fbc094d3ceeac90a
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-09 15:44:58 +0300

    task 2 regex'es finished

--- 2_homework_lesson_08.py
+++ 2_homework_lesson_08.py
@@ -5,8 +5,8 @@ import re
 
 # Валидация ip4 адресов через regex требует особой тщательности
 # <remote_addr> "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}"gm
-# <request_datetime>
-# <request_type> "[A-Z]{3,}\s"gm
-# <requested_resource>
-# <response_code>
-# <response_size>
+# <request_datetime> "\[(.*?)\]"g
+# <request_type> "[A-Z]{3,}\s"gm "\"[A-Z]{3,}"g #второй ааприант лучше
+# <requested_resource> "(\/.{9}/.{9})"g ## очень плохно ориентироваться на кол-во символов здесь
+# <response_code> "\s\d{3}\s"gm
+# <response_size> "(\s\d{3}\s\d+)"gm ## потом взять только вторую часть
