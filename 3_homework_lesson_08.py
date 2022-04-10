6429b1a38b10a6c94981d9076a103efd924965f0
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-10 13:16:25 +0300

    task 3 learning teachers solution and testing code from book

--- 3_homework_lesson_08.py
+++ 3_homework_lesson_08.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 # Урок 8. Декораторы
 # Задание 3
+# разбор решения преподавателя
+from functools import wraps
+def type_logger(func):
+    @wraps(func)
+    def wrapper(*args):
+        for arg in args:
+            print(f'{func.__name__}({arg}: {type(arg)})', end=', ')
+        return func(*args)
 
+    return wrapper
+
+
+@type_logger
+def calc_cube(*args):
+    ''' обязательно импортировать модуль functools'''
+    return list(map(lambda x: x ** 3, args))
+
+a = calc_cube(5, 3)
+print(a)
+print(calc_cube.__name__)
+print(calc_cube.__doc__)
--- testing_lesson_code.py
+++ testing_lesson_code.py
@@ -1,23 +1,27 @@
-import re
-# '\b[\w.-]+?@\w+?\.\w+?\b' - работает в одноязычной среде
-# не ловит ошибки вида: 'ааа@ввв.ыыы'
-# '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)' нашел в Сети
-RE_EMAIL = re.compile(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
+def render_input(field):
+    return f'<input id="id_{field}" type="text" name="{field}">'
 
 
-def email_parse(email_address):
-    """Парсер e-mail"""
-    if RE_EMAIL.match(email_address) != None:
-        result_dict = {}
-        email_address = email_address.split('@')
-        result_dict.update({email_address[0]: email_address[1]})
-    else:
-        # pass
-        raise ValueError
-    return result_dict
+username_f = render_input('username')
+print(username_f)
 
-try:
-    print(email_parse('asd@asd.asd'))
-    print(email_parse('фыв@фыв.фыв'))
-except ValueError as error:
-    print(f'Ошибка: {error}')
+
+def p_wrapper(func):
+    print(func)
+
+
+    def tag_wrapper(*args, **kwargs):
+        print('args', args)
+        print('kwargs', kwargs)
+        markup = func(*args, **kwargs)
+        print(markup)
+        return markup
+
+    return tag_wrapper
+
+@p_wrapper
+def render_input(field):
+    return f'<input id="id_{field}" type="text" name="{field}">'
+
+username_f = render_input('username')
+print(render_input)
