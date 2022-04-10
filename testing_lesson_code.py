1564889b8843a530ee8f9f02669830031cebd33d
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-10 13:49:30 +0300

    learning code from lecture

---
+++ testing_lesson_code.py
@@ -0,0 +1,20 @@
+# def print_human_name(human):
+#     print(human['name'])
+#
+#
+# h1 = {'name': 'QWE'}
+# h2 = {'name': 'ASD'}
+# h3 = {'name': 'ZXC'}
+#
+# print(h1)
+# print(h2)
+# print(h3)
+
+class Phone:
+    def call(self):
+        print('calling')
+
+phone1 = Phone()
+phone2 = Phone()
+phone1.call()
+phone2.call()
