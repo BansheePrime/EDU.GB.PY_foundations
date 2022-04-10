22bc35e76fb421ae2b6d2314d972773f95daf3aa
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-10 12:47:32 +0300

    task 2 done updating .gitignore to exclude parsing results

--- 2_homework_lesson_08.py
+++ 2_homework_lesson_08.py
@@ -17,19 +17,26 @@ import os
 
 REMOTE_ADDR = re.compile(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}')
 REQ_DATETIME = re.compile(r'\[(.*?)\]')
-REQ_TYPE = re.compile(r'([A-Z]{3,})')
+REQ_TYPE = re.compile(r'\"([A-Z]{3,})')
 REQ_RESOURCE = re.compile(r'(\/.{9}\/.{9})')
 REQ_CODE = re.compile(r'\s(\d{3})\s')
 REQ_SIZE = re.compile(r'\d{3}\s(\d+)')
 
 
 def log_parser(raw_line):
-    parsed_data = (re.search(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}', raw_line))
-    return parsed_data
+    ''' Парсер логов по схеме задания '''
+    parsed_data = (REMOTE_ADDR.findall(raw_line), REQ_DATETIME.findall(raw_line), REQ_TYPE.findall(raw_line), REQ_RESOURCE.findall(raw_line), REQ_CODE.findall(raw_line), REQ_SIZE.findall(raw_line))
+    clear_parsed_data = ''
+    for i in parsed_data:
+        clear_parsed_data = clear_parsed_data + str(i).strip('[]')
+    return clear_parsed_data
 
-
-# with open('testing_nginx_logs.txt') as file:
-#     raw_line = file.readline()
-#     parsed_raw = log_parser(raw_line)
-#     with open('parsing_nginx_logs.txt', 'a') as result:
-#         result.write(parsed_raw)
+# raw_line = '188.138.60.101 - - [17/May/2015:08:05:49 +0000] "GET /downloads/product_2 HTTP/1.1" 304 0 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"'
+# print(log_parser(raw_line))
+with open('nginx_logs.txt') as file:
+    for line in file:
+        raw_line = file.readline()
+        parsed_raw = log_parser(raw_line)
+        with open('parsing_nginx_logs.txt', 'a') as result:
+            result.write(parsed_raw)
+            result.write('\n')
