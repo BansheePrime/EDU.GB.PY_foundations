ae27c7d796a7b08375dce107932569c662d83186
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-10 05:41:51 +0300

    task 1 almost done

--- 1_homework_lesson_08.py
+++ 1_homework_lesson_08.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 # Урок 8. Декораторы
 # Задание 1
-
 import re
-
-RE_EMAIL = re.compile(r'\b[\w.-]+?@\w+?\.\w+?\b')
+# '\b[\w.-]+?@\w+?\.\w+?\b' - работает в одноязычной среде
+# не ловит ошибки вида: 'ааа@ввв.ыыы'
+# '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)' нашел в Сети
+RE_EMAIL = re.compile(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
 
 
 def email_parse(email_address):
     """Парсер e-mail"""
-    return RE_EMAIL.match(email_address)
+    if RE_EMAIL.match(email_address) != None:
+        result_dict = {}
+        email_address = email_address.split('@')
+        result_dict.update({email_address[0]: email_address[1]})
+    else:
+        raise ValueError
+    return result_dict
 
 
-if __name__ == '__main__':
-    while True:
-        email_address = input('Введите адрес для парсинга:\n')
-        if email_parse(email_address):
-            break
-    print(f'Результат:{email_address}')
+print(email_parse('asd@asd.asd'))
+print(email_parse('фыв@фыв.фыв'))
--- testing_lesson_code.py
+++ testing_lesson_code.py
@@ -1,27 +1,20 @@
-# letters = {chr(code) for code in range(ord('а'), ord('я') + 1)}
-# letters.add('ё')
-# print(type(letters), str(len(letters)), letters)
+import re
+# '\b[\w.-]+?@\w+?\.\w+?\b' - работает в одноязычной среде
+# не ловит ошибки вида: 'ааа@ввв.ыыы'
+# '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)' нашел в Сети
+RE_EMAIL = re.compile(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
 
-# def name_is_valid(name):
-#     if not name or set(name.lower()) - letters:
-#         return False
 
-# name = ['абырвалг', 'monster', 'авыqwe']
-# for i in name:
-#     result = set(str(name)) - letters
-#     print(result)
+def email_parse(email_address):
+    """Парсер e-mail"""
+    if RE_EMAIL.match(email_address) != None:
+        result_dict = {}
+        email_address = email_address.split('@')
+        result_dict.update({email_address[0]: email_address[1]})
+    else:
+        raise ValueError
+    return result_dict
 
-valid_letters = {chr(sym_code) for sym_code in range(ord('а'), ord('я') + 1)}
-valid_letters.add('ё')
 
-def name_is_valid(name):
-    if not name or set(name.lower()) - set(valid_letters):
-        return False
-    return name.istitle()
-
-if __name__ == '__main__':
-    while True:
-        name = input('Введите имя:\n')
-        if name_is_valid(name):
-            break
-    print(f'пользователь: {name}')
+print(email_parse('asd@asd.asd'))
+print(email_parse('фыв@фыв.фыв'))
