f4f9c48be46b37fc7cfa16be5b6caddff9dd9d93
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-14 09:59:24 +0300

    tasks 4 and 5 learning teachers solution

--- 5_homework_lesson_09.py
+++ 5_homework_lesson_09.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
 # Урок 9. Объектно-ориентированное программирование (ООП)
 # Задание 5
+# разбор решения преподавателя
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print('Ручка пишет')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print('Карандаш рисует')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print('Маркер расчерчивает')
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+
+pen.draw()
+pencil.draw()
+handle.draw()
