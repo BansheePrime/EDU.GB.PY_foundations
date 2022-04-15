912a1ecd358aa9430141d5de9373b896004308e4
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-15 08:16:43 +0300

    testing coloring

--- loto_game_homework_11.py
+++ loto_game_homework_11.py
@@ -3,22 +3,22 @@
 # 
 import random
 
-class bcolors:
+class TermColoring:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
-    ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    col_off = '\033[0m'
 
-print(f"{bcolors.HEADER}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.OKBLUE}Будет работать на unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.OKCYAN}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.OKGREEN}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.WARNING}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.FAIL}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.BOLD}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
-print(f"{bcolors.UNDERLINE}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{TermColoring.HEADER}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.OKBLUE}Будет работать на unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.OKCYAN}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.OKGREEN}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.WARNING}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.FAIL}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.BOLD}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
+print(f"{TermColoring.UNDERLINE}This will work on unixes including OS X, Linux and Windows{TermColoring.col_off}")
