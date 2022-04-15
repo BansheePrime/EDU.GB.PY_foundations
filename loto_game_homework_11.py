e6a1b996e79fc05a644af9746d1e60dd227a5337
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-04-15 07:52:24 +0300

    adding colors

--- loto_game_homework_11.py
+++ loto_game_homework_11.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Урок 11. Игра "Лото"
 # 
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(f"{bcolors.HEADER}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.OKBLUE}Будет работать на unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.OKCYAN}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.OKGREEN}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.WARNING}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.FAIL}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.BOLD}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
+print(f"{bcolors.UNDERLINE}This will work on unixes including OS X, Linux and Windows{bcolors.ENDC}")
